458409e66a501c9e5483975d8b4f9848298d2fc4
Kevin Akos Rankine <kevinranks@gmail.com>
2013-10-11 18:32:56 -0400

    socket.io

--- web.js
+++ web.js
@@ -1,36 +1,44 @@
 #!/usr/bin/env node
 
+var http = require('http');
 var rest = require('restler');
 var express = require('express');
+
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
-function directions(from, to, res) {
-    var reqURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + from.lat + "," + from.lon+ "&destination=" + to.lat + "," + to.lon + "&sensor=false";
-    rest.get(reqURL).on('complete', sendData(res));
-}
-
-function sendData(to) {
-    return function(err, data) {
-	var map = JSON.parse(data.raw);
-	var directions = map.routes[0].legs[0].steps;
-	
-	var instructions = "";
-	for(var i = 0;i < directions.length;i++) {
-            instructions += directions[i].html_instructions + "<br><br>";
-	}
-	
-	to.send(instructions);
-	to.end();
-	console.log("Response ended");
-	return;
-    };
-}
-
-app.get('/', function(req, res) {
+/*app.get('/', function (req, res) {
     console.log("Request received!");
     var from = {"lat" : req.query.from_lat, "lon" : req.query.from_lon};
     var to = {"lat" : req.query.to_lat, "lon" : req.query.to_lon};
-    directions(from, to, res);
+    
+    var reqURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + from.lat + "," + from.lon+ "&destination=" + to.lat + "," + to.lon + "&sensor=false";
+    var instructions = "";
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    rest.get(reqURL).on('complete', function (result) {
+	console.log(result);
+	var map = result;
+	var directions = map.routes[0].legs[0].steps;
+	
+	for(var i = 0;i < directions.length;i++) {
+	    instructions += directions[i].html_instructions + "<br><br>";
+	}
+	res.write(instructions);
+	res.end();
+	console.log(res.headersSent);
+    });
+}); */
+
+app.get('/socket', function (req, res) {
+    res.sendfile('index.html');
 });
 
-app.listen(8080);
+io.sockets.on('connection', function (socket) {
+    socket.emit("hello", {});
+    socket.on('hello', function(data) {
+	socket.emit("hello", {});
+    });
+});
+
+server.listen(8080);
