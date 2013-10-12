e99339e0cd40d2ae8a014c0735edd5a59bf70206
Kevin Akos Rankine <kevinranks@gmail.com>
2013-10-12 21:36:58 +0000

    Kev can know where the bitches at

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ express = require('express'),
 app = express(), 
 server = http.createServer(app),
 routes = require('./routes'),
-io = require('socket.io').listen(server, {log: false });
+io = require('socket.io').listen(server, {log : false});
 
 
 app.configure(function() {
@@ -21,33 +21,48 @@ app.get('/', routes.index);
 server.listen(8080);
 
 var numConnections = 0;
-var coords = {};
 var users = [];
+var coords = {};
 
 io.sockets.on('connection', function (socket) {
-    socket.on('coordinates', function(data) {
-	if (users.indexOf(socket.id) == -1) {
-	    users.push(socket.id);
+    var requestingUser;
+    var requestedUser;
+    var newCoordinates;
+    socket.on('directionsRequested', function(data) {
+	requestingUser = data.requestingUser;
+	requestedUser = data.requestedUser;
+	newCoordinates = data.coordinates;
+	console.log(requestingUser + " wants to know where " + requestedUser + " is. :)");
+	
+	if (users.indexOf(requestingUser) == -1) {
+	    users.push(requestingUser);
+	    setInterval(updateUser, 3000, socket, requestingUser, requestedUser);
 	}
-	coords[socket.id] = data;
-	console.log(coords);
+	coords[requestingUser] = newCoordinates;
     });
     socket.on('disconnect', function () {
-	delete coords[socket.id];
-	users.splice(users.indexOf(socket.id), 1);
+	delete coords[requestingUser];
+	users.splice(users.indexOf(requestingUser), 1);
     });
 });
 
-function updateUsers() {
-    if (users.length == 2) {
-	from = coords[users[0]];
-	to = coords[users[1]];
-	var reqURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + from.lat + "," + from.lon+ "&destination=" + to.lat + "," + to.lon + "&sensor=true&mode=walking";
-	rest.get(reqURL).on('complete', function (result) {
-	    var map = result;
-	    var directions = map.routes[0].legs[0].steps;
-	    io.sockets.emit("directions", directions);
-	});
+function updateUser(socket, requestingUser, requestedUser) {
+    from = coords[requestingUser];
+    to = coords[requestedUser];
+    if (from == undefined) {
+	clearInterval(this);
+	return;
     }
+    if (to == undefined) {
+	console.log("Looks like we ain't sending out shit this round to " + requestingUser);
+	return
+    }
+
+    var reqURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + from.lat + "," + from.lon+ "&destination=" + to.lat + "," + to.lon + "&sensor=true&mode=walking";
+    rest.get(reqURL).on('complete', function (result) {
+	var map = result;
+	var directions = map.routes[0].legs[0].steps;
+	socket.emit("directions", directions);
+	console.log("We sent out directions from " + requestingUser + " to " + requestedUser);
+    });
 }
-setInterval(updateUsers, 2000);
